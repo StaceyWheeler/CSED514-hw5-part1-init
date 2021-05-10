7660420b29425d803e87229e9050afb280468c0c
emi90 <emily.yamauchi90@gmail.com>
2021-05-10 12:01:51 -0700

    updated sql file

--- hw5_part1_sql.sql
+++ hw5_part1_sql.sql
@@ -5,7 +5,7 @@ Create Table Caregivers(
 	CaregiverName varchar(50)
 	);
 
-Create Table AppointmentStatusCodes(
+/*Create Table AppointmentStatusCodes(
 	StatusCodeId int PRIMARY KEY,
 	StatusCode   varchar(30)
 	);
@@ -20,16 +20,15 @@ INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
 	VALUES (3, 'Completed');
 INSERT INTO AppointmentStatusCodes (statusCodeId, StatusCode)
 	VALUES (4, 'Missed');
+*/
 
-Create Table VaccineAppointment(
-	VaccineAppointmentId int PRIMARY KEY,
-	StatusCodeId int  DEFAULT 0 NOT NULL
-		CONSTRAINT FK_AppointmentStatusCode FOREIGN KEY (StatusCodeId) 
-		     REFERENCES AppointmentStatusCodes(StatusCodeId)
+Create Table Patients(
+	PatientID int PRIMARY KEY,
+	PatientName varchar(150)
 	);
 
-Create Table CareGiverSchedule(
-	CaregiverSlotSchedulingId int Identity PRIMARY KEY, 
+Create Table CaregiverSchedule(
+	SlotID int Identity PRIMARY KEY, 
 	CaregiverId int DEFAULT 0 NOT NULL
 		CONSTRAINT FK_CareGiverScheduleCaregiverId FOREIGN KEY (caregiverId)
 			REFERENCES Caregivers(CaregiverId),
@@ -37,31 +36,45 @@ Create Table CareGiverSchedule(
 	--SlotTime time,
 	SlotHour int DEFAULT 0 NOT NULL,
 	SlotMinute int DEFAULT 0 NOT NULL,
-	SlotStatus int  DEFAULT 0 NOT NULL
-		CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
-		     REFERENCES AppointmentStatusCodes(StatusCodeId),
-	VaccineAppointmentId int DEFAULT 0 NOT NULL
-		CONSTRAINT FK_CaregiverAppointmentCode FOREIGN KEY (VaccineAppointmentId)
-			REFERENCES VaccineAppointment(VaccineAppointmentId)
+	--SlotStatus int  DEFAULT 0 NOT NULL
+		--CONSTRAINT FK_CaregiverStatusCode FOREIGN KEY (SlotStatus) 
+		     --REFERENCES AppointmentStatusCodes(StatusCodeId),
+	--VaccineAppointmentId int DEFAULT 0 NOT NULL
+		--CONSTRAINT FK_CaregiverAppointmentCode FOREIGN KEY (VaccineAppointmentId)
+			--REFERENCES VaccineAppointment(VaccineAppointmentId)
+	--PatientCount int DEFAULT 0 NOT NULL CHECK (PatientCount < 4),
 	);
 
 Create Table Vaccines(
-	VaccineId int,
-	VaccineName varchar(50),
-	VaccineAppointmentId int DEFAULT 0 NOT NULL
-		CONSTRAINT FK_VaccineAppointmentCode FOREIGN KEY (VaccineAppointmentId)
-			REFERENCES VaccineAppointment(VaccineAppointmentId)
-	PRIMARY KEY (VaccineId, VaccineName)
+	VaccineID int PRIMARY KEY,
+	VaccineBrand varchar(50), --Moderna, Pfizer, etc. 
+	SlotStatus varchar(50)  --DEFAULT 0 NOT NULL
+		--CONSTRAINT FK_StatusCode FOREIGN KEY (SlotStatus) 
+		     --REFERENCES AppointmentStatusCodes(StatusCodeId),
+	--VaccineAppointmentId int DEFAULT 0 NOT NULL
+		--CONSTRAINT FK_VaccineAppointmentCode FOREIGN KEY (VaccineAppointmentId)
+			--REFERENCES VaccineAppointment(VaccineAppointmentId), 
 	);
 --https://stackoverflow.com/questions/1750335/can-sql-table-have-multiple-columns-with-primary-key/1750347#:~:text=If%20you%20mean%20%22can%20a,%22%2C%20the%20answer%20is%20yes.&text=A%20table%20can%20have%20just,col1%2C%20col2%2C%20col3)%20)%3B
 
-Create Table Patients(
-	PatientId int PRIMARY KEY,
-	PatientName varchar(150),
-	VaccineAppointmentId int DEFAULT 0 NOT NULL
-		CONSTRAINT FK_PatientAppointmentCode FOREIGN KEY (VaccineAppointmentId)
-			REFERENCES VaccineAppointment(VaccineAppointmentId)
+
+Create Table VaccineAppointment(
+	VaccineAppointmentID int PRIMARY KEY,
+	PatientID int  DEFAULT 0 NOT NULL
+		CONSTRAINT FK_VaccineAppointmentPatientID FOREIGN KEY (PatientID) 
+		     REFERENCES Patients(PatientID),
+	SlotID int DEFAULT 0 NOT NULL
+		CONSTRAINT FK_VaccineAppointmentSlotID FOREIGN KEY (SlotID) 
+		     REFERENCES CaregiverSchedule(SlotID),
+	VaccineID int NOT NULL
+		CONSTRAINT FK_VaccineAppointmentVaccineID FOREIGN KEY (VaccineID)
+			REFERENCES Vaccines(VaccineID)
 	);
+
+
+
+
+
 
 -- Additional helper code for your use if needed
 
