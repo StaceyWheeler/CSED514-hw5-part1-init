9141f9c728ad5a515d38b08f2f1cea5ad229ee83
emi90 <emily.yamauchi90@gmail.com>
2021-05-13 21:31:19 -0700

    updated tests

--- test_week1.py
+++ test_week1.py
@@ -119,6 +119,48 @@ class TestCOVID19_vaccine(unittest.TestCase):
                     clear_tables(sqlClient)
                     self.fail("Failed to verify reserved doses")
 
+    def test_reserve_dose_insufficient(self):
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                DBname=os.getenv("DBName"),
+                                UserId=os.getenv("UserID"),
+                                Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new Moderna Vaccine object
+                    self.moderna = covid('Moderna', 2, 28, cursor)
+                    sqlQuery = '''
+                            SELECT AvailableDoses
+                            FROM Vaccines
+                            WHERE VaccineName = 'Moderna'
+                            '''
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+                    # Add 1 dose of Moderna
+                    self.moderna.AddDoses('Moderna', 1, cursor)
+                    # Reserve 2 doses of Moderna -- this does not execute
+                    self.moderna.ReserveDoses('Moderna', cursor)
+                    # check if doses are reserved
+                    sqlQuery2 = '''
+                            SELECT AvailableDoses
+                            FROM Vaccines
+                            WHERE VaccineName = 'Moderna'
+                            '''
+                    cursor.execute(sqlQuery2)
+                    rows2 = cursor.fetchall()
+                    before_reserve = rows[0]
+                    after_reserve = rows2[0]
+                    print('before: ', before_reserve)
+                    print('after: ', after_reserve)
+                    if before_reserve['AvailableDoses'] == after_reserve['AvailableDoses']:
+                        self.fail("Failed to verify reserved doses")
+                    # clear the tables after testing, just in-case
+                    clear_tables(sqlClient)
+                except Exception:
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    self.fail("Failed to verify reserved doses")
 
 if __name__ == '__main__':
     unittest.main()
